1d68d632a0ab8687ec58559094b2438169dd1ca8
Lenka <lenocka369@gmail.com>
2019-12-13 11:11:04 +0100

    Simulate calls for user service

---
+++ resources/User.py
@@ -0,0 +1,12 @@
+# # Simulate calls for user service
+import os
+
+from resources.resource import Resource, read_from_dir
+
+
+class User:
+    @staticmethod
+    def list(session):
+        # Lists all users.
+
+        return [bool(1), read_from_dir(Resource.CLUSTER), int(0)]
